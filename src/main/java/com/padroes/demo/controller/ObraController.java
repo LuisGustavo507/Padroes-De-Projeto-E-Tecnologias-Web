7b52a48ffdae630fb1c40deaa3e54f36abe5400c
luisGustavo507 <luizingustavo507@gmail.com>
2025-10-23 21:57:23 -0300

    feat: continuacao

--- src/main/java/com/padroes/demo/controller/ObraController.java
+++ src/main/java/com/padroes/demo/controller/ObraController.java
@@ -1,6 +1,8 @@
 package com.padroes.demo.controller;
 
+import com.padroes.demo.model.Obra;
 import com.padroes.demo.model.Usuario;
+import com.padroes.demo.repository.ObraRepository;
 import com.padroes.demo.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,29 +13,29 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/usuario")
-public class UsuarioController {
+@RequestMapping("/obra")
+public class ObraController {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private ObraRepository obraRepository;
 
 
     @Autowired
     @RequestMapping(value = "novo", method = RequestMethod.POST)
-    public ResponseEntity<Usuario> novo(@RequestBody Usuario usuario){
-        return new ResponseEntity<Usuario>(usuarioRepository.save(usuario), HttpStatus.OK);
+    public ResponseEntity<Obra> novo(@RequestBody Obra obra){
+        return new ResponseEntity<Obra>(obraRepository.save(obra), HttpStatus.OK);
     }
 
     @Autowired
     @RequestMapping(value = "listar", method = RequestMethod.GET)
-    public ResponseEntity<List<Usuario>> listar(){
-        return ResponseEntity.ok((List<Usuario>) usuarioRepository.findAll());
+    public ResponseEntity<List<Obra>> listar(){
+        return ResponseEntity.ok((List<Obra>) obraRepository.findAll());
     }
 
     @Autowired
     @RequestMapping(value = "listar/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Usuario> getById(@PathVariable(value = "id") Long id){
-        Optional<Usuario> object = usuarioRepository.findById(id);
+    public ResponseEntity<Obra> getById(@PathVariable(value = "id") Long id){
+        Optional<Obra> object = obraRepository.findById(id);
         if(object.isPresent()){
             return new ResponseEntity<>(object.get(), HttpStatus.OK);
         }else{
@@ -44,8 +46,8 @@ public class UsuarioController {
     @Autowired
     @DeleteMapping(value = "/deletar/{id}")
     public ResponseEntity<Void> deletar(@PathVariable(value = "id") Long id){
-        if(usuarioRepository.existsById(id)){
-            usuarioRepository.deleteById(id);
+        if(obraRepository.existsById(id)){
+            obraRepository.deleteById(id);
             return ResponseEntity.noContent().build();
         }else {
             return  ResponseEntity.notFound().build();
--- src/main/java/com/padroes/demo/repository/ObraRepository.java
+++ src/main/java/com/padroes/demo/repository/ObraRepository.java
@@ -1,10 +1,9 @@
 package com.padroes.demo.repository;
 
-import com.padroes.demo.model.Usuario;
+import com.padroes.demo.model.Obra;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+public interface ObraRepository extends JpaRepository<Obra, Long> {
 }
